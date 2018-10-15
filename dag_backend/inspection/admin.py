38b2e76c15492bbc6a1e567a5797406eab000d19
malep2007 <ephraim.malinga@gmail.com>
2018-10-15 22:04:15 +0300

    implemented customer module

--- dag_backend/inspection/admin.py
+++ dag_backend/inspection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from inspection.models import PropertyDetail, Surface
+
+class PropertyDetailAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'property_type', 'services_required',)
+
+class SurfaceAdmin(admin.ModelAdmin):
+    list_display = ('surfaces', 'surface_area',)
+
+admin.site.register(Surface, SurfaceAdmin)
+admin.site.register(PropertyDetail, PropertyDetailAdmin)
 
 # Register your models here.
