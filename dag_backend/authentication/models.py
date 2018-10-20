1289a7a52a75b4ae195d30f3cb77e05afdb3fd2d
malep2007 <ephraim.malinga@gmail.com>
2018-10-20 11:51:22 +0300

    modified travis file

--- dag_backend/authentication/models.py
+++ dag_backend/authentication/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class DagUserModel(models.Model):
-    username = models.CharField(max_length=20)
