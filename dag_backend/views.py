a89782ca331b2d48b3e9bf616c7173eaa0a72284
Ephraim Malinga <ephraim.malinga@gmail.com>
2019-01-10 10:21:46 +0300

    modified layout views

--- dag_backend/views.py
+++ dag_backend/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html", {"title":"Home Page"})
+    return render(request, "index.html", { "title": "Home"})
