df924c34d015b8d6b0764c095dbcd27c489ba827
Ephraim Malinga <ephraim.malinga@gmail.com>
2019-01-11 10:14:49 +0300

    changed database configuration

--- dag_backend/settings.py
+++ dag_backend/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dag_and_bragan',
-        'USER': 'daguser',
-        'PASSWORD': '123',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
